5421e213934680ae2b16aff95d1b18b61ea0688b
seungdori_home <eofeh4@gmail.com>
2024-05-10 02:19:18 +0900

    Revise and cleanup code
    add ratio compare funciton

--- Predict/predict.py
+++ Predict/predict.py
@@ -40,8 +40,7 @@ def calculate_angle_arm(a, b, c):
     c = np.array(c)  # 세번째
 
     # 라디안을 계산하고 실제 각도로 변경한다.
-    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - \
-        np.arctan2(a[1]-b[1], a[0]-b[0])
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
     angle_arm = np.abs(radians*180.0/np.pi)
 
     # 180도가 넘으면 360에서 뺀 값을 계산한다.
@@ -68,10 +67,12 @@ pre_gesture = 'N'
 count_gesture = 0
 
 shape_hand = 'N'
+final_hand = 'N'
 depth_hand = None
 active_hands = None
 
 angle_arm = 0
+ratio_arm=0
 keypoints_count = 9  # Number of array for pose coordinate
 keypoint_indices = {
     0: 6,  # Nose
@@ -106,6 +107,44 @@ while True:
     depth_nose = None
     
     array_keypoints = np.zeros((keypoints_count, 2))  # [RS,RE,RW,LS,LE,LW,]
+    
+    #################### Predict hands ####################
+    results_hands = model_hands(color_image, conf=0.8, verbose=False)  
+    if results_hands is not None:
+        for r in results_hands:
+            boxes = r.boxes  # Boxes class
+            final_hands_index = 0
+            min_box_depth = float('inf')
+            list_depth_boxes = [0] * len(boxes)
+            list_shape_hands = [''] * len(boxes)
+            x1, y1, x2, y2=0,0,0,0
+            
+            #Check front box
+            for number_box, box in enumerate(boxes):  
+                b = box.xyxy[0].to('cpu').detach().numpy().copy()
+                x1, y1, x2, y2 = map(int, b[:4])# Box left top and right bottom coordinate
+                box_cx, box_cy = int((x2 - x1) / 2 + x1), int((y2 - y1) / 2 + y1)
+                depth_box = depth_frame.get_distance(box_cx, box_cy)
+                list_depth_boxes[number_box] = depth_box
+                list_shape_hands[number_box] = model_hands.names[int(box.cls)]
+                
+                # Drawing bounding box
+                if (depth_box!=0 and depth_box < min_box_depth):
+                    min_box_depth = depth_box
+                    final_hands_index = number_box
+
+            #select active hand
+            if len(boxes) > 0:  # Ensure index is within bounds
+                x1, y1, x2, y2 = map(int, boxes[final_hands_index].xyxy[0].to('cpu').detach().numpy().copy())
+                box_cx, box_cy = int((x2 - x1) / 2 + x1), int((y2 - y1) / 2 + y1)
+                shape_hand = list_shape_hands[final_hands_index]
+                depth_hand = list_depth_boxes[final_hands_index]
+
+                cv2.rectangle(color_image, (x1, y1), (x2, y2),(0, 0, 255), thickness=2, lineType=cv2.LINE_4)
+                cv2.putText(color_image, f"Depth: {depth_hand}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+                cv2.putText(color_image, shape_hand, (box_cx, box_cy), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                # print("find min hands:",shape_hand)
+                # print(f"Closest Hand Gesture: {shape_hand} at Index {final_hands_index} with Depth {min_box_depth}")
 
     #################### Predict pose ####################
     results_pose = model_pose(color_image, conf=0.8, verbose=False)  # Predict pose
@@ -170,78 +209,37 @@ while True:
             value_sry = int(array_keypoints[0][1])
             value_hry = int(array_keypoints[7][1])
             value_hly = int(array_keypoints[8][1])
+    ###################################################################################
 
-    #################### Predict hands ####################
-    results_hands = model_hands(pose_color_image, conf=0.8, verbose=False)  
-    if results_hands is not None:
-        for r in results_hands:
-            boxes = r.boxes  # Boxes class
-            final_hands_index = 0
-            min_box_depth = float('inf')
-            list_depth_boxes = [0] * len(boxes)
-            list_shape_hands = [''] * len(boxes)
-            x1, y1, x2, y2=0,0,0,0
-            # list_coordi_boxes =[0,0,0,0]
-            
-            #Check front box
-            for number_box, box in enumerate(boxes):  
-                b = box.xyxy[0].to('cpu').detach().numpy().copy()
-                x1, y1, x2, y2 = map(int, b[:4])# Box left top and right bottom coordinate
-                box_cx, box_cy = int((x2 - x1) / 2 + x1), int((y2 - y1) / 2 + y1)
-                depth_box = depth_frame.get_distance(box_cx, box_cy)
-                list_depth_boxes[number_box] = depth_box
-                list_shape_hands[number_box] = model_hands.names[int(box.cls)]
-                
-                # Drawing bounding box
-                if depth_box < min_box_depth:
-                    min_box_depth = depth_box
-                    final_hands_index = number_box
-                    x1, y1, x2, y2 = map(int, b[:4])
+    #Distinction between left and right hands
+    if box_cx is not None and box_cy is not None:
+        distance_whr = np.sqrt((box_cx - int(array_keypoints[2][0]))**2 + (box_cy - int(array_keypoints[2][1]))**2)
+        distance_whl = np.sqrt((box_cx - int(array_keypoints[5][0]))**2 + (box_cy - int(array_keypoints[5][1]))**2)
+        
+        # Activate hand selection
+        if distance_whl is not None and distance_whr is not None:  
+            if (distance_whl > distance_whr):
+                active_hands = 'RIGHT'
+                angle_arm = calculate_angle_arm(array_keypoints[0], array_keypoints[1], array_keypoints[2])
+            elif (distance_whl < distance_whr):
+                active_hands = 'LEFT'
+                angle_arm = calculate_angle_arm(array_keypoints[3], array_keypoints[4], array_keypoints[5])
 
-                cv2.rectangle(pose_color_image, (x1, y1), (x2, y2),(0, 0, 255), thickness=2, lineType=cv2.LINE_4)
-                cv2.putText(pose_color_image, f"Depth: {depth_hand}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
-                cv2.putText(pose_color_image, list_shape_hands[number_box], (box_cx, box_cy), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-            #Set active hand
-            if len(boxes) > 0:  # Ensure index is within bounds
-                x1, y1, x2, y2 = map(int, boxes[final_hands_index].xyxy[0].to('cpu').detach().numpy().copy())
-                shape_hand = list_shape_hands[final_hands_index]
-                depth_hand = list_depth_boxes[final_hands_index]
-
-    #             # print("find min hands:",shape_hand)
-    #             # print(f"Closest Hand Gesture: {shape_hand} at Index {final_hands_index} with Depth {min_box_depth}")
-
-    if shape_hand == 'STOP':
-        cur_stop_cx, cur_stop_cy = int(
-            (x2 - x1) / 2 + x1), int((y2 - y1) / 2 + y1)  # Set current box coordinate
-        if pre_stop_cx is not None and pre_stop_cy is not None:
-            distance_stop = abs(
-                cur_stop_cx - pre_stop_cx)  # Get distance using x
-            # print(distance_stop)
-
-            if distance_stop > threshold_waving:  # Check sharply movement using only x value
-                shape_hand = 'W'
-            else:
-                shape_hand = 'S'
-
-        pre_stop_cx, pre_stop_cy = cur_stop_cx, cur_stop_cy
-
-    elif shape_hand == 'POINTING':
-        shape_hand = shape_hand[0]
-        pbox_cx, pbox_cy = box_cx, box_cy
-    else:
-        shape_hand = shape_hand[0]
-    # #Distinction between left and right hands
-    # if box_cx is not None:
-    #             distance_whr = np.sqrt((box_cx - int(array_keypoints[2][0]))**2 + (box_cy - int(array_keypoints[2][1]))**2)
-    #             distance_whl = np.sqrt((box_cx - int(array_keypoints[5][0]))**2 + (box_cy - int(array_keypoints[5][1]))**2)
-    # if distance_whl is not None and distance_whr is not None:  # Activate hand selection
-    #     if (distance_whl > distance_whr):
-    #         active_hands = 'RIGHT'
-    #         angle_arm = calculate_angle_arm(array_keypoints[0], array_keypoints[1], array_keypoints[2])
-    #     elif (distance_whl < distance_whr):
-    #         active_hands = 'LEFT'
-    #         angle_arm = calculate_angle_arm(array_keypoints[3], array_keypoints[4], array_keypoints[5])
+        # Get ratio between shoulder-hip and shoulder-box
+        if (active_hands == 'RIGHT' and value_hry is not None and value_sry is not None):
+            if(value_hry>0 and value_sry>0):
+                diff_y_sh = value_hry-value_sry
+                diff_y_sb = abs(box_cy-value_sry)
+                # print(diff_y_sb/diff_y_sh, 'H: ', value_hry,
+                #     'S: ', value_sry, 'B: ', box_cy)
+        elif (active_hands == 'LEFT' and value_hly is not None and value_sly is not None):
+            if(value_hly>0 and value_sly>0):
+                diff_y_sh = value_hly-value_sly
+                diff_y_sb = abs(box_cy-value_sly)
+                # print(diff_y_sb/diff_y_sh, 'H: ', value_hly,
+                #     'S: ', value_sly, 'B: ', box_cy)
+        ratio_arm=diff_y_sb/diff_y_sh
+   
     # #Get nose depth
     # try: 
     #     depth_nose = depth_frame.get_distance(int(array_keypoints[6][0]), int(array_keypoints[6][1]))
@@ -249,7 +247,6 @@ while True:
     #         array_keypoints[6][1])), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_4)
     # except RuntimeError as e:
     #     print(f"An error occurred: {e}")
-
     # #Get distance between nose and hand
     # if (depth_nose is not None and depth_hand is not None):
     #     if (depth_nose != 0.0 and depth_hand != 0.0):
@@ -260,22 +257,31 @@ while True:
     #                 if (box_cy < y1 or box_cy > y2 or box_cx < x1 or box_cx > x2):
     #                     print("Misrecognition gesture out of the box.\n")
     #                     gesture = 'N'
-    # # Get ratio between shoulder-hip and shoulder-box
-    # if (active_hands == 'RIGHT' and value_hry is not None and value_sry is not None):
-    #     if(value_hry>0 and value_sry>0):
-    #         diff_y_sh = value_hry-value_sry
-    #         diff_y_sb = abs(box_cy-value_sry)
-    #         print(diff_y_sb/diff_y_sh, 'H: ', value_hry,
-    #             'S: ', value_sry, 'B: ', box_cy)
-    # elif (active_hands == 'LEFT' and value_hly is not None and value_sly is not None):
-    #     if(value_hly>0 and value_sly>0):
-    #         diff_y_sh = value_hly-value_sly
-    #         diff_y_sb = abs(box_cy-value_sly)
-    #         print(diff_y_sb/diff_y_sh, 'H: ', value_hly,
-    #             'S: ', value_sly, 'B: ', box_cy)
+    
     box_cx, box_cy = None, None
-    # #----------------------------------------------------------------------------------------#
+    ratio_hand=shape_hand
+    if(ratio_hand=='FORWARD' and angle_arm>150 and ratio_arm>0.5):
+        final_hand=ratio_hand[0]
+        print(final_hand)
+        
+    #convert 1 character
+    if shape_hand == 'STOP':
+        shape_hand = shape_hand[0]
+        cur_stop_cx, cur_stop_cy = int((x2 - x1) / 2 + x1), int((y2 - y1) / 2 + y1)  # Set current box coordinate
+        if pre_stop_cx is not None and pre_stop_cy is not None:
+            distance_stop = abs(cur_stop_cx - pre_stop_cx)  # Get distance using x
+            # print(distance_stop)
+            if distance_stop > threshold_waving:  # Check sharply movement using only x value
+                shape_hand = 'W'
 
+        pre_stop_cx, pre_stop_cy = cur_stop_cx, cur_stop_cy
+    elif shape_hand == 'POINTING':
+        shape_hand = shape_hand[0]
+        pbox_cx, pbox_cy = box_cx, box_cy
+    else:
+        shape_hand = shape_hand[0]
+
+    #3times validation
     gesture_this = shape_hand
     if gesture_this == 'P' and pbox_cx is not None:
         if active_hands == 'RIGHT' and distance_whr is not None and value_srx is not None:
@@ -295,17 +301,13 @@ while True:
         if (count_gesture > 3):
             count_gesture = 0
             gesture = gesture_this
-            # print('gesture: ',gesture)
+            if gesture != 'N':
+                # print(gesture)
+                # ser.write(str(gesture).encode('utf-8'))
+                gesture = 'N'
     else:
         pre_gesture = gesture_this
 
-    if gesture != 'N':
-        # print(gesture)
-        # print(gesture, angle_arm)
-        # ser.write(str(gesture).encode('utf-8')) #To do
-        gesture = 'N'
-
-    # cv2.imshow("predict", color_image)  # 주석 처리된 부분은 필요에 따라 활성화할 수 있습니다.
     cv2.imshow("predict", pose_color_image)  # 주석 처리된 부분은 필요에 따라 활성화할 수 있습니다.
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
